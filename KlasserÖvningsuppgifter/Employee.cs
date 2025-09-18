7ccba4312ead53640ce908f5907464d0560e5da0
Jontiq <99jonathancar@gmail.com>
2025-09-18 10:49:58 +0200

    Add project files.

---
+++ KlasserÖvningsuppgifter/Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasserÖvningsuppgifter
+{
+    internal class Employee
+    {
+        //Properties
+        private string _name;
+        public string Name { get; set; }
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _age = 0;
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        private decimal _salary;
+        public decimal Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _salary = 0;
+                }
+                else
+                {
+                    _salary = value;
+                }
+            }
+        }
+
+        //Konstruktör
+        public Employee(string name, int age, decimal salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+
+        //Metoder
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Salary: {Salary:C}");
+        }
+    }
+}
---
+++ KlasserÖvningsuppgifter/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace KlasserÖvningsuppgifter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee worker1 = new Employee("Alice", 30, 50000m);
+
+            worker1.DisplayEmployeeInfo();
+        }
+    }
+}
